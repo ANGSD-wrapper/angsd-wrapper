e65ebc7d283759323ddc474f4711fdda8e8871d9
Arun Durvasula <ohblahitsme@yahoo.com>
2014-10-05 18:57:05 -0700

    Squashed 'angsd/' content from commit ccc544f
    git-subtree-dir: angsd
    git-subtree-split: ccc544fc28e71b496a71b4fbeda190699dc52d24

---
+++ abcMajorMinor.h
@@ -0,0 +1,15 @@
+
+
+class abcMajorMinor:public abc{
+  int doMajorMinor;
+  int doSaf;
+  char *pest;
+public:
+  void printArg(FILE *argFile);
+  void run(funkyPars *pars);
+  void clean(funkyPars *pars);  
+  void print(funkyPars *pars);  
+  abcMajorMinor(const char *outfiles,argStruct *arguments,int inputtype);
+  void getOptions(argStruct *arguments);
+  ~abcMajorMinor();
+};
