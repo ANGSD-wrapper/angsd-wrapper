445b861e52f7e83590c5c22b33114d93d70e4362
Tom Kono <konox006@umn.edu>
2014-11-29 16:16:09 -0600

    More Config and init cleanup

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-mkdir ./results/ ./temp/ ./data/
+#   Script to create the directory structure for angsd-wrapper
+source common.conf
+#   test if our directory is writable (it should be!)
+if [ -w `dirname ${PROJECT_DIR}` ]
+    then mkdir -p ${RESULTS_DIR} ${TEMP_DIR} ${DATA_DIR}
+    else echo "Directory is not writable!"; exit 1
+fi
