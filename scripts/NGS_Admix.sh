0b6b4d1cecad2f7ddeea849b7b08326c08ab5316
Arun Durvasula <ohblahitsme@yahoo.com>
2015-03-09 17:29:57 -0700

    added ngs admix script not tested

---
+++ scripts/NGS_Admix.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+#   source the common config file
+source scripts/common.conf
+# load utils functions
+source ${SCRIPTS_DIR}/utils.sh
+
+K=5
+N_CORES=32
+MIN_MAF=0.05
+
+load_config $1
+
+for k in {2..$K}
+	${ngsPopGen}/NGSadmix \
+		-likes ${LIKELIHOOD}\
+		-K ${k}\
+		-P ${N_CORES}\
+		-o ${TAXON}\
+		-minMaf ${MIN_MAF}
+done
