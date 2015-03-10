4755a6ccedce357c54aec438050cb516e45f8532
Arun Durvasula <ohblahitsme@yahoo.com>
2015-03-09 21:51:08 -0700

    added SFS plotting

--- scripts/shiny/ui.R
+++ scripts/shiny/ui.R
@@ -4,42 +4,109 @@ shinyUI(fluidPage(
   
   # Application title
   titlePanel("angsd-wrapper graph"),
-
-  sidebarLayout(
-    sidebarPanel(
-        fileInput('userThetas',
-                  label= 'Choose Thetas File'
-        ),
-        
-        selectInput("thetaChoice",
-                    label = "Choose estimator of theta to graph", 
-                    choices = c("Watterson's Theta", "Pairwise Theta", "Fu and Li's Theta", "Fay's Theta", "Maximum likelihood (L) Theta"),
-                    selected = "Watterson's Theta"
-        ),
-        selectInput("selectionChoice",
-                    label = "Choose a neutrality test statistic to graph", 
-                    choices = c("Tajima's D", "Fi and Li's D", "Fu and Li's F", "Fay and Wu's H", "Zeng's E"),
-                    selected = "Tajima's D"
-        ),
-        hr(),
-        checkboxInput("thetaLowess","Theta Lowess", value=FALSE),
-        checkboxInput("selectionLowess","Neutrality Test Statistic Lowess", value=FALSE),
-        hr(),
-        numericInput("WinCenterLow", "Base Start Position", value=0),
-        numericInput("WinCenterHigh", "Base End Position", value=10000),
-        checkboxInput("subset","Toggle subset data", value=FALSE),
-        
-        hr(),
-        fileInput('userAnnotations',
-                  label= 'Choose GFF File'
-        ),
-        checkboxInput("annotations","Toggle GFF annotations", value=FALSE)
+  tabsetPanel(
+    tabPanel(
+      "Thetas",
+      sidebarLayout(
+        sidebarPanel(
+            fileInput('userThetas',
+                      label= 'Choose Thetas File'
+            ),
+            
+            selectInput("thetaChoice",
+                        label = "Choose estimator of theta to graph", 
+                        choices = c("Watterson's Theta", "Pairwise Theta", "Fu and Li's Theta", "Fay's Theta", "Maximum likelihood (L) Theta"),
+                        selected = "Watterson's Theta"
+            ),
+            selectInput("selectionChoice",
+                        label = "Choose a neutrality test statistic to graph", 
+                        choices = c("Tajima's D", "Fi and Li's D", "Fu and Li's F", "Fay and Wu's H", "Zeng's E"),
+                        selected = "Tajima's D"
+            ),
+            hr(),
+            checkboxInput("thetaLowess","Theta Lowess", value=FALSE),
+            checkboxInput("selectionLowess","Neutrality Test Statistic Lowess", value=FALSE),
+            hr(),
+            numericInput("WinCenterLow", "Base Start Position", value=0),
+            numericInput("WinCenterHigh", "Base End Position", value=10000),
+            checkboxInput("subset","Toggle subset data", value=FALSE),
+            
+            hr(),
+            fileInput('userAnnotations',
+                      label= 'Choose GFF File'
+            ),
+            checkboxInput("annotations","Toggle GFF annotations", value=FALSE)
+          ),
+        # Show a plot of the thetas
+          mainPanel(
+              plotOutput("thetaPlot"), 
+              plotOutput("selectionPlot")
+          )
+        )
       ),
-    # Show a plot of the thetas
-      mainPanel(
-        plotOutput("thetaPlot"),
-        plotOutput("selectionPlot")
+    tabPanel(
+      "SFS",
+      sidebarLayout(
+        sidebarPanel(
+          fileInput('userSFS',
+                    label= 'Choose SFS File'
+          )
+          
+        ),
+        mainPanel(
+          plotOutput("SFSPlot")
+        )
       )
+    ),
+    tabPanel(
+      "ABBA BABA",
+      sidebarLayout(
+        sidebarPanel(
+          
+          
+        ),
+        mainPanel(
+          
+        )
+      )
+    ),
+    tabPanel(
+      "Fst",
+      sidebarLayout(
+        sidebarPanel(
+          
+          
+        ),
+        mainPanel(
+          
+        )
+      )
+    ),
+    tabPanel(
+      "PCA",
+      sidebarLayout(
+        sidebarPanel(
+          
+          
+        ),
+        mainPanel(
+          
+        )
+      )
+    ),
+    tabPanel(
+      "Admixture",
+      sidebarLayout(
+        sidebarPanel(
+          
+          
+        ),
+        mainPanel(
+          
+        )
+      )
+    )
+    #end of tabsetPanel
     )
   )
 )
