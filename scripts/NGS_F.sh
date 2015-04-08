8f029af13969b9ee2a5329fc593a60c928fabf40
Arun Durvasula <adurvasu@farm.cse.ucdavis.edu>
2015-04-08 11:17:48 -0700

    fixed ngsF end of line space issue

--- scripts/NGS_F.sh
+++ scripts/NGS_F.sh
@@ -18,17 +18,17 @@ DO_MAJORMINOR=1
 DO_MAF=1
 SNP_PVAL=1e-6
 OVERRIDE=false
-N_IND=`wc -l < ${TAXON_LIST}`
 SEED=12345
 MIN_EPSILON=1e-9
 
 # load variables from supplied config file
 load_config $1
+N_IND=`wc -l < ${TAXON_LIST}`
 
 if [[ ${REGIONS} == */* ]]; then
 	${ANGSD_DIR}/angsd \
-		-bam ${TAXON_LIST}\ 
-		-rf ${REGIONS}\ 
+		-bam ${TAXON_LIST}\
+		-rf ${REGIONS}\
 		-doGLF ${DO_GLF}\
 		-GL ${GT_LIKELIHOOD}\
 		-out ${RESULTS_DIR}/${TAXON}\
@@ -36,14 +36,14 @@ if [[ ${REGIONS} == */* ]]; then
 		-anc ${ANC_SEQ}\
 		-doMaf ${DO_MAF}\
 		-SNP_pval ${SNP_PVAL}\
-		-doMajorMinor ${DO_MAJORMINOR}\ 
+		-doMajorMinor ${DO_MAJORMINOR}\
 		-minMapQ ${MIN_MAPQ}\
 		-minQ ${MIN_BASEQUAL}\
 		-nThreads ${N_CORES}
 else
-		${ANGSD_DIR}/angsd \
-		-bam ${TAXON_LIST}\ 
-		-r ${REGIONS}\ 
+       	${ANGSD_DIR}/angsd \
+		-bam ${TAXON_LIST}\
+		-r ${REGIONS}\
 		-doGLF ${DO_GLF}\
 		-GL ${GT_LIKELIHOOD}\
 		-out ${RESULTS_DIR}/${TAXON}\
@@ -51,10 +51,11 @@ else
 		-anc ${ANC_SEQ}\
 		-doMaf ${DO_MAF}\
 		-SNP_pval ${SNP_PVAL}\
-		-doMajorMinor ${DO_MAJORMINOR}\ 
+		-doMajorMinor ${DO_MAJORMINOR}\
 		-minMapQ ${MIN_MAPQ}\
 		-minQ ${MIN_BASEQUAL}\
 		-nThreads ${N_CORES}
+fi
 
 N_SITES=$((`zcat ${TAXON}.HWE.mafs.gz | wc -l`-1))
 
@@ -64,9 +65,9 @@ zcat ${RESULTS_DIR}/${TAXON}.glf.gz | ./ngsF/ngsF \
 	-n_sites ${N_SITES}\
 	-min_epsilon ${MIN_EPSILON}\
 	-glf -\
-	-out ${RESULTS_DIR}/${TAXON}.approx_indF\ 
+	-out ${RESULTS_DIR}/${TAXON}.approx_indF\
 	-approx_EM\
-	-seed ${SEED}\ 
+	-seed ${SEED}\
 	-init_values r\
 	-n_threads ${N_CORES}
 
@@ -75,6 +76,7 @@ zcat ${RESULTS_DIR}/${TAXON}.glf.gz | ./ngsF/ngsF \
 	-n_sites ${N_SITES}\
 	-min_epsilon ${MIN_EPSILON}\
 	-glf -\
-	-out ${RESULTS_DIR}/${TAXON}.indF\ 
+	-out ${RESULTS_DIR}/${TAXON}.indF\
 	-init_values ${RESULTS_DIR}/${TAXON}.approx_indF.pars\
 	-n_threads ${N_CORES}
+
