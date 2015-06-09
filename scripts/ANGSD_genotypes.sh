f1a7207f32a005ae32cb0f2e7c671c3072198235
Arun Durvasula <ohblahitsme@yahoo.com>
2015-06-09 15:15:22 -0700

    added defaults to genotypes

--- scripts/ANGSD_genotypes.sh
+++ scripts/ANGSD_genotypes.sh
@@ -7,6 +7,19 @@ source scripts/common.conf
 # load utils functions
 source ${SCRIPTS_DIR}/utils.sh
 
+DO_MAJORMINOR=1
+UNIQUE_ONLY=0
+MIN_MAPQ=30
+MIN_BASEQUAL=20
+GT_LIKELIHOOD=2
+DO_GENO=7
+DO_POST=1
+POST_CUTOFF=0.95
+DO_MAF=2
+SNP_PVAL=1e-6
+MIN_IND=1
+N_CORES=32
+
 load_config $1
 
 N_IND=`wc -l < ${TAXON_LIST}`
@@ -15,20 +28,17 @@ N_IND=`wc -l < ${TAXON_LIST}`
 ${ANGSD_DIR}/angsd\
   -bam ${TAXON_LIST}\
   -out ./results/${TAXON}_snps\
-  -doMajorMinor 1\
-  -uniqueOnly 0\
-  -minMapQ 30\
-  -minQ 20\
-  -GL 2\
-  -r 12:\
-  -doGeno 7\
-  -doPost 1\
-  -postCutoff 0.95\
-  -doMaf 2\
-  -SNP_pval 1e-6\
+  -doMajorMinor ${DO_MAJORMINOR}\
+  -uniqueOnly ${UNIQUE_ONLY}\
+  -minMapQ ${MIN_MAPQ}\
+  -minQ ${MIN_BASEQUAL}\
+  -GL ${GT_LIKELIHOOD}\
+  -r ${REGIONS}\
+  -doGeno ${DO_GENO}\
+  -doPost ${DO_POST}\
+  -postCutoff ${POST_CUTOFF}\
+  -doMaf ${DO_MAF}\
+  -SNP_pval ${SNP_PVAL}\
   -nInd ${N_IND}\
-  -minInd 4\
-  -P 16
-#  -ref ${REF_SEQ}\
-#  -anc ${ANC_SEQ}
-#  -baq 1
+  -minInd ${MIN_IND}\
+  -P ${N_CORES}
