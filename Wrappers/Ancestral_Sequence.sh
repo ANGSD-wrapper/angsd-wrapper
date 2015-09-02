e5974b2a19b0e717e7c016f1fd6db759571b4f52
Paul Hoffman <hoff0792@umn.edu>
2015-09-02 10:57:03 -0500

    Modifications to the Ancestral_Sequence wrapper

--- Wrappers/Ancestral_Sequence.sh
+++ Wrappers/Ancestral_Sequence.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-set -u
 set -o pipefail
 
 #   Load variables from supplied config file
@@ -16,7 +15,38 @@ fi
 #   Where is angsd-wrapper located?
 SOURCE=$2
 
- "${SOURCE}"/dependencies/angsd/angsd \
-    -i "${ANC_BAM}"\
-    -out "${OUT}" \
-    -doFasta "${DO_FASTA}"
+#   Where is ANGSD located?
+ANGSD_DIR=${SOURCE}/dependencies/angsd
+
+#   Create the fasta file
+#       Check to see if we're using -doCounts
+if [[ -z "${DO_COUNTS}" ]]
+then
+    "${ANGSD_DIR}"/angsd \
+        -doFasta "${DO_FASTA}" \
+        -i "${ANC_BAM}"\
+        -out "${OUT}"
+else
+    "${ANGSD_DIR}"/angsd \
+        -doFasta "${DO_FASTA}" \
+        -doCounts "${DO_COUNTS}" \
+        -i "${ANC_BAM}"\
+        -out "${OUT}"
+fi
+
+#   If we have SAMTools, might as well index
+if `command -v samtools > /dev/null 2> /dev/null`
+then
+    echo "Indexing fasta file..."
+    #   Do we need to unzip the fasta file?
+    find "${OUT}" -name "${OUTNAME}.fa*" | grep .gz > /dev/null 2> /dev/null
+    if [[ "$?" -eq 0 ]]
+    then
+        gzip -d "${OUT}".fa.gz
+    fi
+    samtools index "${OUT}".fa
+else
+    echo "Can't find SAMTools, we aren't indexing for you"
+    echo "If you'd like to index yourself,"
+    echo "Then install SAMTools and run the following command:"
+    echo "samtools faidx ${OUT}/${OUTNAME}.fa
