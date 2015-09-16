c39fc150f24b17fd86739a6289768c8340d014d3
Tyler Kent <tvkent@ucdavis.edu>
2015-09-16 16:46:22 -0400

    Update utils.sh
    had the variable check backwards

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -31,14 +31,14 @@ directory_exists() {
   fi
 }
 
-variable_exists() {
+variable_doesnt_exist() {
   if [[ -z "$1"]]
   then
     return 0
-    echo $1" exists!"
+    echo $1" does not exist!"
   else
     return 1
-    echo $1" does not exist!"
+    echo $1" exists!"
   fi
 }
 
