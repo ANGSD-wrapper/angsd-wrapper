a9163cbfea8194bdb4e07bd66b240067303b47ae
Tyler Kent <tvkent@ucdavis.edu>
2015-09-16 16:39:28 -0400

    Update utils.sh
    added variable exists check for -r regions

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -31,6 +31,17 @@ directory_exists() {
   fi
 }
 
+variable_exists() {
+  if [[ -z "$1"]]
+  then
+    return 0
+    echo $1" exists!"
+  else
+    return 1
+    echo $1" does not exist!"
+  fi
+}
+
 load_config() {
 
   source $1
