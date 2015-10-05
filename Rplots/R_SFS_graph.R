492c12ecb01a272c578a6ed809dc955c705643de
Paul Hoffman <hoff0792@umn.edu>
2015-10-05 11:36:16 -0500

    Generalized Rscript for plotting SFS

--- Rplots/R_SFS_graph.R
+++ Rplots/R_SFS_graph.R
@@ -1,24 +1,20 @@
+#!/usr/bin/env Rscript
+
 # An SFS graphing utility for angsd-wrapper
-
-#   Get initial data
-#   Do not transverse data frame!
-#   The following dataset is my Inversions dataset including 12 samples
-setwd(dir = "/Users/chaochihliu/Dropbox/ANGSD Wrapper/SFS/")
-Derived <- as.matrix(read.table("ANGSD_DerivedSFS", header = FALSE))   #   read in table as matrix
-Derived <- as.data.frame(t(Derived))  #   coerced to matrix before switching rows and columns
-names(Derived) <- c("Allele_frequency")   #   assign column name to dataframe
-
-#   Creating SFS graphs
-#   Bar plot
-barplot(Derived[2:24,], xlab = "# of variants at site", ylab = "Proportion of SNPs", main = "Site Frequency Spectrum",
-        col = "purple4")
-
-#   Histogram
-#   Make histogram excluding row 1 and 25, this needs to be fixed in the wrapper script later on
-hist(Derived[2:24,], right = FALSE, xlab = "# of variants at site", 
-     ylab = "Proportion of SNPs",main = "Site Frequency Spectrum", col = "deepskyblue")
-
-#   Plot
-plot(Derived[2:24,], type = "p", xlab = "# of variants at site",
-     ylab = "Proportion of SNPs", main = "Site Frequency Spectrum", col = "navyblue", pch = 18)
-  
+plot.sfs <- function(sfs) {
+    #   Get initial data
+    #   Do not transverse data frame!
+    Derived <- as.matrix(read.table(sfs, header = FALSE))   #   read in table as matrix
+    Derived <- as.data.frame(t(Derived))  #   coerced to matrix before switching rows and columns
+    names(Derived) <- c("Allele_frequency")   #   assign column name to dataframe
+    #   Get rid of first and last values for graphing purposes
+    Derived <- Derived[3:nrow(x = Derived) - 1, ]
+    #   Creating SFS graphs
+    #   Bar plot
+    barplot(Derived, xlab = "# of variants at site", ylab = "Proportion of SNPs", main = "Site Frequency Spectrum", col = "purple4")
+    #   Histogram
+    #   Make histogram excluding row 1 and 25, this needs to be fixed in the wrapper script later on
+    hist(Derived, right = FALSE, xlab = "# of variants at site", ylab = "Proportion of SNPs",main = "Site Frequency Spectrum", col = "deepskyblue")
+    #   Plot
+    plot(Derived, type = "p", xlab = "# of variants at site", ylab = "Proportion of SNPs", main = "Site Frequency Spectrum", col = "navyblue", pch = 18)
+}
