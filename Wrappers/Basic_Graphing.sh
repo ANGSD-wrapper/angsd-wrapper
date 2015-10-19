659c53720016d23348aba4431b4ec4167af5f2d3
Paul Hoffman <hoff0792@umn.edu>
2015-10-19 10:42:35 -0500

    Implemented Thetas method for Basic Graphing

--- Wrappers/Basic_Graphing.sh
+++ Wrappers/Basic_Graphing.sh
@@ -30,10 +30,20 @@ then
 fi
 
 #   Generate plots for each variable supplied
-
 #   Site Frequency Spectrum
 if ! [[ -z "${SFS_OUT}" ]]
 then
+    echo "Plotting SFS..."
     SFS_NAME=`basename "${SFS_OUT}" _DerivedSFS`
     Rscript "${GRAPH_DIR}"/SFS_plot.R "${SFS_OUT}" "${OUT}" "${SFS_NAME}"
+    echo "Thetas plot can be found at ${OUT}/${SFS_NAME}_SFS.pdf"
+fi
+
+#   Thetas
+if ! [[ -z "${THETAS_OUT}" ]]
+then
+    echo "Plotting Thetas..."
+    THETAS_NAME=`basename "${THETAS_OUT}" .pest.pg`
+    Rscript "${GRAPH_DIR}"/Thetas_plot.R "${THETAS_OUT}" "${OUT}" "${THETAS_NAME}"
+    echo "Thetas plot can be found at ${OUT}/${THETAS_NAME}_Thetas.pdf"
 fi
