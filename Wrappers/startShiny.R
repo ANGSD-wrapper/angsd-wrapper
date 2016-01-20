29f5855748fb5d09a364fd5bcdccc4aa1a023cb7
Paul Hoffman <hoff0792@umn.edu>
2016-01-20 16:01:53 -0600

    Whoops

--- Wrappers/startShiny.R
+++ Wrappers/startShiny.R
@@ -11,7 +11,7 @@ pkgTest <- function(package) {
 }
 
 #   A list of required pacakges
-pkgList <- c("shiny", "lattice", "Hmisc", "ape", "data.table", "DT", "shinythemes", "dplyr")
+pkgList <- c("shiny", "lattice", "Hmisc", "ape", "data.table", "DT", "shinythemes")
 
 #   Load the packages
 batchInstall <- function(pkgList) {
