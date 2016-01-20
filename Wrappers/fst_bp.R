2f0ae22c7ec9d381d13a3735debbe316618779fb
Paul Hoffman <hoff0792@umn.edu>
2016-01-20 16:06:24 -0600

    Now, we can load our required packages

--- Wrappers/fst_bp.R
+++ Wrappers/fst_bp.R
@@ -1,6 +1,4 @@
 #!/usr/bin/env Rscript
-library(data.table)
-library(dplyr)
 
 # Collect arguments from the command line
 args <- commandArgs(trailingOnly = TRUE)
@@ -9,6 +7,23 @@ fstOut <- args[2]
 mafs.g1 <- args[3]
 mafs.g2 <- args[4]
 graph.me.fst <- args[5]
+
+# Install dependent libraries
+pkgTest <- function(package) {
+    if(package %in% rownames(installed.packages()) == FALSE) { # check to see if a packages is available
+        install.packages(package) # if not, insall it
+    }
+}
+
+#   A list of required pacakges
+pkgList <- c("data.table", "dplyr")
+
+#   Load the packages
+options(repos = c(CRAN = "http://cran.rstudio.com")) # set a repo mirror, we used RStudio just because
+for(dep in pkgList) { # Iterate through each dependent package
+    pkgTest(dep) # Test to see if it's installed
+}
+lapply(X = pkgList, FUN = library, character.only = TRUE) # load the packages to be used
 
 # Define headers
 fst.headers <- c("A", "AB", "f", "FST", "Pvar")
