4d37f9141c9eec01c30765fbecb9907c41f2a1ef
liux1299 <liux1299@umn.edu>
2016-01-20 15:02:40 -0600

    Incorporating .mafs files into fst graphing

--- Wrappers/fst_bp.R
+++ Wrappers/fst_bp.R
@@ -1,11 +1,14 @@
 #!/usr/bin/env Rscript
 library(data.table)
+library(dplyr)
 
 # Collect arguments from the command line
 args <- commandArgs(trailingOnly = TRUE)
 intersectOut <- args[1]
 fstOut <- args[2]
-graph.me.fst <- args[3]
+mafs.g1 <- args[3]
+mafs.g2 <- args[4]
+graph.me.fst <- args[5]
 
 # Define headers
 fst.headers <- c("A", "AB", "f", "FST", "Pvar")
@@ -13,10 +16,15 @@ intersect.headers <- c("Chr","bp")
 
 # Read in data from .fst file and shared.pos file
 intersect <- fread(intersectOut, sep = "\t", col.names = intersect.headers)
-fst <- read.table(fstOut, sep = "\t", col.names = fst.headers)
+fst <- fread(fstOut, sep = "\t", col.names = fst.headers %>%
+               select(c("Pvar")))
+mafs1 <- fread(mafs.g1 %>%
+                 select(c(chromo, position, major, minor, knownEM)))
+mafs2 <- fread(mafs.g2 %>%
+                 select(c(chromo, position, major, minor, knownEM)))
 
 # Combine two datasets
-FST <- cbind(intersect, fst)
+FST <- cbind(intersect, fst, mafs1, mafs2)
 
 # Save to .fst file
 write.table(FST, graph.me.fst, col.names = F, row.names = F)
