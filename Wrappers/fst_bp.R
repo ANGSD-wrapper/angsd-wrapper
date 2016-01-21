6c7b45abc3366ccdb76132efcdc025a6f10efd4c
liux1299 <liux1299@umn.edu>
2016-01-21 17:45:45 -0600

    Updates to .fst filtering

--- Wrappers/fst_bp.R
+++ Wrappers/fst_bp.R
@@ -15,7 +15,7 @@ pkgTest <- function(package) {
     }
 }
 
-#   A list of required pacakges
+#   A list of required packages
 pkgList <- c("data.table", "dplyr")
 
 #   Load the packages
@@ -27,19 +27,29 @@ lapply(X = pkgList, FUN = library, character.only = TRUE) # load the packages to
 
 # Define headers
 fst.headers <- c("A", "AB", "f", "FST", "Pvar")
-intersect.headers <- c("Chr","bp")
+intersect.headers <- c("Chr","position")
 
-# Read in data from .fst file and shared.pos file
-intersect <- read.table(file = intersectOut, sep = "\t", col.names = intersect.headers)
-fst <- read.table(file = fstOut, sep = "\t", col.names = fst.headers %>%
-               select(c("Pvar")))
-mafs1 <- read.table(file = mafs.g1 %>%
-                 select(c(chromo, position, major, minor, knownEM)))
-mafs2 <- read.table(file= = mafs.g2 %>%
-                 select(c(chromo, position, major, minor, knownEM)))
+# Read in data from .fst file, shared.pos file, and two mafs files from 2 selected populations
+# Subset data and select columns to pull
+intersect <- fread(input = intersectOut, sep = "\t", col.names = intersect.headers)
+fst <- fread(input = fstOut, sep = "\t", col.names = fst.headers)
+fst <- subset(x = fst, select = c("Pvar"))
+mafs1 <- fread(input = mafs.g1)
+mafs1 <- subset(x = mafs1, select = c("chromo", "position", "major", "minor", "knownEM"))
+mafs2 <- fread(input = mafs.g2)
+mafs2 <- subset(x = mafs2, select = c("chromo", "position", "major", "minor", "knownEM"))
+
+# Merge mafs1 and mafs2
+mafs.1and2 <- merge(mafs1, mafs2, by = "position", suffixes = c(".mafs1", ".mafs2"))
 
 # Combine two datasets
-FST <- cbind(intersect, fst, mafs1, mafs2)
+FST <- cbind(intersect, fst)
+
+# Merge FST dataset and mafs.1and2 dataset and filter by columns
+data.fst <- merge(mafs.1and2, FST, by = "position")
+filter.knownEM <- subset(x = data.fst, knownEM.mafs1 + knownEM.mafs2 > 0)
+filter.minor <- subset(x = filter.knownEM, minor.mafs2 == minor.mafs1 | minor.mafs2 == major.mafs1)
+filter.major <- subset(x = filter.minor, major.mafs2 == major.mafs1 | major.mafs2 == minor.mafs1)
 
 # Save to .fst file
-write.table(x = FST, file = graph.me.fst, col.names = F, row.names = F)
+write.table(x = filter.major, file = "maize.graph.me.fst", col.names = F, row.names = F)
