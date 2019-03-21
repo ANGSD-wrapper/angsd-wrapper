a8f21d489f1026208e5fe1087591816f51d6f623
Chaochih Liu <liux1299@umn.edu>
2019-03-21 13:54:04 -0500

    Remove deprecated features

--- Wrappers/FST.sh
+++ Wrappers/FST.sh
@@ -18,9 +18,6 @@ SOURCE=$2
 #   Where is ANGSD?
 ANGSD_DIR="${SOURCE}"/dependencies/angsd
 
-#   Where is ngsFST?
-NGS_POPGEN="${SOURCE}"/dependencies/ngsPopGen
-
 #   Create the out directory
 OUT="${SCRATCH}"/"${PROJECT}"/Fst
 mkdir -p "${OUT}"
@@ -231,58 +228,3 @@ if [[ -n "${WIN}" ]] && [[ -n "${STEP}" ]]; then
 else
     echo "WRAPPER: windowed fst not requested..." >&2
 fi
-
-#########################################################################
-# Deprecated
-#########################################################################
-#   Estimate the Fst using ngsFST
-#   First, convert our 2D SFS output to the old output for ngsPopGen
-#echo "WRAPPER: converting 2D SFS for Fst Estimations..." >&2
-#"${ANGSD_DIR}"/misc/realSFS print \
-#    "${OUT}"/"${GROUP_1}"_Intergenic.saf.idx \
-#    "${OUT}"/"${GROUP_2}"_Intergenic.saf.idx \
-#    -P "${N_CORES}" \
-#    -oldout 1
-
-#   Move the shared.pos.gz file to our out directory
-#mv ${OUT/2DSFS}/shared.pos.gz ${OUT}
-
-#   Unzip shared.pos.gz and get the number of shared sites
-#gzip -df ${OUT}/shared.pos.gz
-#N_SITES=`wc -l < "${OUT}"/shared.pos`
-
-#   Generate a prior spectrum using ngs2dSFS from ngsPopGen
-#echo "WRAPPER: generating spectrum..." >&2
-#${NGS_POPGEN}/ngs2dSFS \
-#    -postfiles ${OUT}/${GROUP_1}_Intergenic.saf ${OUT}/${GROUP_2}_Intergenic.saf \
-#    -outfile ${OUT}/${GROUP_1}.${GROUP_2}.spectrum.txt \
-#    -nind ${N_IND1} ${N_IND2} \
-#    -relative ${RELATIVE} \
-#    -maxlike ${MAX_LIKE} \
-#    -block_size ${BLOCK_SIZE} \
-#    -nsites ${N_SITES}
-
-#   Calculate Fst using ngsFST
-#echo "WRAPPER: estimating Fst..." >&2
-#${NGS_POPGEN}/ngsFST \
-#    -postfiles ${OUT}/${GROUP_1}_Intergenic.saf ${OUT}/${GROUP_2}_Intergenic.saf \
-#    -priorfile ${OUT}/${GROUP_1}.${GROUP_2}.spectrum.txt \
-#    -nind ${N_IND1} ${N_IND2} \
-#    -block_size ${BLOCK_SIZE} \
-#    -nsites ${N_SITES} \
-#    -outfile ${OUT}/${GROUP_1}.${GROUP_2}.fst
-
-#   Unzip the mafs files
-#gzip -df ${OUT}/${GROUP_1}_Intergenic.mafs.gz
-#gzip -df ${OUT}/${GROUP_2}_Intergenic.mafs.gz
-
-#   Merge shared.pos file with Fst output file
-#echo "WRAPPER: creating files for Shiny graphing..." >&2
-#Rscript ${SOURCE}/Wrappers/fst_bp.R \
-#    ${SOURCE} \
-#    ${OUT}/shared.pos \
-#    ${OUT}/${GROUP_1}.${GROUP_2}.fst \
-#    ${OUT}/${GROUP_1}_Intergenic.mafs \
-#    ${OUT}/${GROUP_2}_Intergenic.mafs \
-#    ${OUT}/"${PROJECT}".Fst.graph.me
-# why do we not zip the mafs again? sure this takes time, but the unzipped files are very large
